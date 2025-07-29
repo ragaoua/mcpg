2f4882e96155e611bd44cb29823d9396db1b40ed
Reda AGAOUA <reda.agaoua@gmail.com>
2025-07-29 22:20:06 +0200

    flag.StringVar used instead of flag.String

--- cmd/serve_data/main.go
+++ cmd/serve_data/main.go
@@ -11,9 +11,8 @@ import (
 func main() {
 	db_url, var_exists := os.LookupEnv("DB_URL")
 	if !var_exists {
-		db_url_ptr := flag.String("db-url", "", "URL to connect to the cluster db")
+		flag.StringVar(&db_url, "db-url", "", "URL to connect to the cluster db")
 		flag.Parse()
-		db_url = *db_url_ptr
 
 		if db_url == "" {
 			log.Fatalf("Variable DB_URL or option --db-url must be set")
